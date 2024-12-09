5d44832de64ecae648fc49297afc8cdc909a86d1
matt-baugh <mb4617@ic.ac.uk>
2024-12-09 10:12:18 +0000

    day 8

---
+++ 2024/day08/task1.py
@@ -0,0 +1,51 @@
+from utils.file_utils import load_file
+
+
+def construct_map(content: list[str]):
+    freq_map = {}
+    for i, l in enumerate(content):
+        for j, c in enumerate(l):
+            if c != '.':
+                if c not in freq_map:
+                    freq_map[c] = []
+
+                freq_map[c].append((i, j))
+
+    return freq_map
+
+
+def count_antinodes(filename_str: str) -> int:
+    content = load_file(filename_str)
+
+    num_rows = len(content)
+    num_cols = len(content[0])
+
+    freq_map = construct_map(content)
+    print(freq_map)
+
+    antinode_locs = set()
+
+    for n_list in freq_map.values():
+        for i, (n1_i, n1_j) in enumerate(n_list):
+            for n2_i, n2_j in n_list[i + 1:]:
+                diff_i = n2_i - n1_i
+                diff_j = n2_j - n1_j
+
+                for new_i, new_j in [(n2_i + diff_i, n2_j + diff_j), (n1_i - diff_i, n1_j - diff_j)]:
+
+                    if new_i >= 0 and new_i < num_rows and new_j >= 0 and new_j < num_cols:
+                        antinode_locs.add((new_i, new_j))
+
+        # print(c)
+        # print(len(antinode_locs), ':', antinode_locs)
+        # print()
+
+    return len(antinode_locs)
+
+
+if __name__ == "__main__":
+
+    print(count_antinodes("data1_test.txt"))
+    # start = time()
+    # print(count_antinodes("data1_real.txt"))
+    # print("Execution time:", time() - start)
---
+++ 2024/day08/tmp.py
@@ -0,0 +1,69 @@
+
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+# aj
+# bi
+# ch
+
+# for i, curr_thing in enumerate(a):
+#     print(curr_thing + a[len(a) - 1 - i])
+
+# for front_elem, back_elem in zip(a, reversed(a)):
+#     print(front_elem + back_elem)
+
+# print(list(reversed(a)))
+
+
+# print(list(zip([1, 2, 3], [4, 5, 6], [7, 8, 9])))
+
+# t = ('a', 'j')
+
+# print([front_elem + back_elem for (front_elem, back_elem) in zip(a, reversed(a))])
+
+# print(a[5:8])
+
+b = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+
+# for key, value in b.items():
+#     print(key, value)
+
+# freq_map = {
+#     '0': [(0, 0), ]
+# }
+
+
+# print(a)
+
+# a.append('k')
+
+# print(a)
+
+# d = ([(8, 90)], 2, 3)
+
+# coordinate = tuple[int]
+
+# dict[str, list[tuple[int, int]]]
+
+# print(b)
+
+# print('Keys')
+# for elem in b.keys():
+#     print(elem)
+
+# print('Values')
+# for elem in b.values():
+#     print(elem)
+
+# print('Items')
+# for elem in b.items():
+#     print(elem)
+
+print(a)
+
+a.append([1, 2, 3])
+
+print(a)
