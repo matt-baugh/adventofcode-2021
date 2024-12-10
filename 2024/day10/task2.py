3a90e0c58c426b7fcf27ed91fe6d6ac596beed91
matt-baugh <mb4617@ic.ac.uk>
2024-12-10 10:43:56 +0000

    day 10

---
+++ 2024/day10/task2.py
@@ -0,0 +1,61 @@
+from time import time
+
+import task1
+
+from utils.file_utils import load_file
+
+
+def walk_to_9(coord: tuple[int, int], top_map: list[list[int]], leads_to_map: dict[tuple[int, int], int]) -> int:
+
+    if coord in leads_to_map:
+        return leads_to_map[coord]
+
+    i, j = coord
+
+    height = top_map[i][j]
+
+    if height == 9:
+        reachable = 1
+        leads_to_map[coord] = 1
+        return reachable
+
+    reachable = 0
+    for i_diff, j_diff in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        new_i = i + i_diff
+        new_j = j + j_diff
+
+        if new_i >= 0 and new_i < len(top_map) and new_j >= 0 and new_j < len(top_map[0]) and top_map[new_i][new_j] == (height + 1):
+            reachable += walk_to_9((new_i, new_j), top_map, leads_to_map)
+
+    leads_to_map[coord] = reachable
+    return reachable
+
+
+def sum_scores(filename_str: str) -> int:
+    content = load_file(filename_str)
+
+    top_map = [[-1 if c == '.' else int(c) for c in l]for l in content]
+
+    start_coords = []
+    for i, row in enumerate(top_map):
+        for j, h in enumerate(row):
+            if h == 0:
+                start_coords.append((i, j))
+
+    leads_to_map = {}
+    scores = 0
+    for c in start_coords:
+        scores += walk_to_9(c, top_map, leads_to_map)
+
+    # print(leads_to_map)
+
+    return scores
+
+
+if __name__ == "__main__":
+
+    print(sum_scores("data1_test.txt"))
+    start = time()
+    print('t1: ', task1.sum_scores("data1_real.txt"))
+    print('t2: ', sum_scores("data1_real.txt"))
+    print("Execution time:", time() - start)
